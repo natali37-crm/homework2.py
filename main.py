34d5ba6614f87ba5507dead4996edcd058127bea
natali37-crm <natal.filimonova2016@yandex.ru>
2024-06-26 00:06:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+number=('всего задач 12')
+print(number)
+time=('затрачено времени 1.5')
+print(time)
+lesson=('курс Python')
+print(lesson)
+print(1.5/12)
+averge=('среднее время 0.125')
+print(averge)
+
+print(lesson,number,time,averge)
+
+
+
+
+
+
+
